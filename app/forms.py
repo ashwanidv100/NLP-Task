79f96b44e8a901947144dc8bdd028aaee170aacb
Ashwani Kumar <ashwanidv100@gmail.com>
2018-11-25 11:36:33 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,8 @@
+"""
+Class definition for text web form
+
+@author: Ashwani Kumar
+"""
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField
 from wtforms.validators import DataRequired
