74f154f8f0441c9c2e3f9dbf52ffbe4c54074571
Ashwani Kumar <ashwanidv100@gmail.com>
2018-11-25 13:22:31 +0530

    adding to render the initial main page

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask , render_template , request
+
 app = Flask(__name__)
  
-@app.route("/")
-@app.route('/index')
-def hello():
-  return "Hello World!"
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def initial():
+    # render the initial main page
+    return render_template('index.html',
+                           title = 'NLP - Text input')
