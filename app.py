2aa225a12478374fa230c58e2f6f47ed3689b505
Ashwani Kumar <ashwanidv100@gmail.com>
2018-11-26 12:39:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import os
-import logging
-import redis
-import gevent
-from flask import Flask, render_template
-from flask_sockets import Sockets
-
+from flask import Flask, flash, redirect, render_template, request, session, abort
+ 
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return render_template('base.html')
+ 
+@app.route("/")
+def index():
+    return render_template(
+        'test.html')
+ 
+if __name__ == "__main__":
+    app.run()
