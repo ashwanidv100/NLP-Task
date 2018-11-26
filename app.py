6b1f50d51ff8c08f0f2a3e7da96ee5af5d020f1c
Ashwani Kumar <ashwanidv100@gmail.com>
2018-11-26 21:59:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
  
 app = Flask(__name__)
- 
+
+def analysetext(processtext):
+    sid = SentimentIntensityAnalyzer()
+    output = sid.polarity_scores(processtext)
+    return output
+	
 @app.route("/")
 def index():
-    return render_template(
-        'test.html')
- 
+    return render_template('test.html')
+		
+@app.route("/", methods = ['POST'])	
+def result():
+    text = request.form['inputText']
+    output = analysetext(text)
+    return render_template('test.html',output= output)
+
 if __name__ == "__main__":
     app.run()
