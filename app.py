927effa57ce372d68af505424798fe745cce262e
Ashwani Kumar <ashwanidv100@gmail.com>
2018-11-27 16:39:51 +0530

    added more changes for running on heroku server

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, flash, redirect, render_template, request
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
+import os
  
 app = Flask(__name__)
 
@@ -20,4 +20,5 @@ def result():
     return render_template('test.html',output= output)
 
 if __name__ == "__main__":
-    app.run()
+    myport = int(os.environ.get("PORT", 5000))
+    app.run(port= myport)
